b1b23c00d638f242c1742988e04c9bd36098c64c
OlgaZueva98 <olga.zueva.1998@mail.ru>
2016-10-18 21:38:10 +0300

    Прогр.

---
+++ HW3/1. a и b в сумме дают c.py
@@ -0,0 +1,12 @@
+print ('Введите a: ') 
+a=int(input())
+print ('Введите b: ')
+b=int(input())
+print ('Введите с: ')
+c=int(input())
+if c==a+b:
+    print ('Да')
+if c!=a+b:
+    print ('Нет')
+
+    
---
+++ HW3/4. c является решением линейного уравнения ax + b = 0.py
@@ -0,0 +1,11 @@
+print ('Введите a: ')
+a=int(input())
+print ('Введите b: ')
+b=int(input())
+print ('Введите с: ')
+c=int(input())
+x=-b/a
+if c==x:
+    print('Да')
+if c!=x:
+    print('Нет')
