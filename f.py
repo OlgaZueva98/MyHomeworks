3836b3cf3e9680acd6a250f1f96498804744b7b5
OlgaZueva98 <olga.zueva.1998@mail.ru>
2017-01-10 13:24:59 +0300

    Add files via upload

---
+++ f.py
@@ -0,0 +1,64 @@
+##import random
+##
+##def random_searh():
+##    a = []
+##    for i in range(20):
+##        random_number = random.randint(1, 100)
+##        a.append(random_number)
+##    length_a = len(a)
+##    print(lengh_a)
+##
+##    max_elem = a[0]
+##    for elem in range(len(a)):
+##        if a[elem] > max_elem:
+##            max_elem = a[elem]
+##            
+##            
+##    for elem in range(len(a)):
+##        if a[elem] == max_elem:
+##            print(elem)
+##    print(max_elem)
+##    return length_a
+##
+##
+##
+##
+
+##def fibonacci(number):
+##    if number == 1 or number == 2:
+##        return 1
+##    return fibonacci(number - 1) + fibonacci(number - 2)
+##
+##number = int(input('Введите число: '))
+##print(fibonacci(number))
+##
+
+##def reverse_arr():
+##    x = input('Введите число: ')
+##    if x != '.':
+##        print(x, 'новая функция')
+##        reverse_arr()
+##    print(x)
+##
+##reverse_arr()
+
+
+
+
+
+##def factorial(x):
+##    if x == 1:
+##        return 1
+##    return (x * factorial(x - 1))
+##x = int(input('Введите число: '))
+##print(factorial(x))
+
+
+
+
+
+
+
+
+
+
